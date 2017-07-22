e33e672613bf624bca7cbd9304894404cea86fb3
nainemom <nainemom@gmail.com>
2017-07-22 19:32:18 +0430

    updated

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import framevuerk from 'framevuerk/dist/framevuerk-fa.js'
 import moment from 'moment-jalaali'
 import 'font-awesome/css/font-awesome.css'
 import './index.html'
+import style from './style.scss'
 import _main from './pages/_main'
 import login from './pages/login'
 import main from './pages/main'
+import user from './pages/user'
 import qs from 'qs'
 import axios from 'axios'
 
@@ -30,6 +32,11 @@ const router = new VueRouter({
       component: main
     },
     {
+      name: 'user',
+      path: '/:id',
+      component: user
+    },
+    {
       name: 'notfound',
       path: '/*'
     }
@@ -38,7 +45,8 @@ const router = new VueRouter({
 
 const ENV = process.env.NODE_ENV || 'development';
 const SSO_URL = process.env.SSO_URL || (ENV == 'development'? 'https://radtest.pec.ir/sso/v1/oauth2/token': 'https://ssoapi.pec.ir/v1/oauth2/token');
-const CLIENT_ID = process.env.CLIENT_ID || ('b9dc712c952b4aafb481abede0fec4d8');
+const CLIENT_ID = process.env.CLIENT_ID || 'b9dc712c952b4aafb481abede0fec4d8';
+const SERVER_URL = process.env.SERVER_URL || (ENV=='development'? 'http://192.168.95.210:8100': 'http://45.33.105.162:5206');
 
 const session = JSON.parse( global.localStorage.getItem('me') );
 const guestUser = {
@@ -48,15 +56,17 @@ const guestUser = {
 export default new Vue({
   data () {
     return {
+      serverUrl: SERVER_URL,
       mainClass: ['fv-col-lg-10', 'fv-col-offset-lg-1', 'fv-col-xl-8', 'fv-col-offset-xl-2'],
       mainClassMin: ['fv-col-sm-8', 'fv-col-offset-sm-2', 'fv-col-md-6', 'fv-col-offset-md-3'],
       me: session === null? guestUser: session,
       loading: false,
       axios: axios.create({
-        baseURL: '/api',
+        baseURL: SERVER_URL,
         timeout: 120000,
         headers: {
           token: null,
+          "Content-Type": "application/json",
           "cache-control": "no-cache"
         }
       }),
@@ -102,7 +112,7 @@ export default new Vue({
         failed();
       });
     },
-    logout (force=false) {
+    logout () {
       this.me = guestUser;
       global.localStorage.removeItem('me');
       this.setToken();
@@ -112,18 +122,62 @@ export default new Vue({
       this.$root.axios.defaults.headers.Authorization = this.me.access_token;
     },
     routeChange(){
-      // if (!this.$refs.sidebar.pPin) {
-      //   this.$refs.sidebar.close()
-      // }
-
       if( this.me.access_token === null ){
         this.logout();
       }
       else if( this.$route.name === 'login' || this.$route.name === 'notfound' ){
         this.$router.push(`/`);
       }
+    },
+    catchHandle(err){
+      this.loading = false;
+      switch( err.response.status ){
+        case 401:
+          this.logout();
+          alert('ابتدا باید وارد سیستم شوید!');
+          break;
+        case 400:
+          alert('با توجه به مقادیر ورودی، انجام این درخواست ممکن نیست!');
+          break;
+        default:
+          alert(err.response.data);
+      }
+    },
+    dateFormat(dt, hr=true){
+      /*var ts = new Date(dt).getTime() / 1000;
+      console.log(ts);*/
+      if( !dt ){
+        return '---'
+      }
+      const mom = moment(dt);
+      return mom.format(`jDD-jMM-jYYYY${hr? ' ساعت HH:mm': ''}`);
+    },
+    statusFormat(v){
+      let icon;
+      let cls;
+      let text;
+      switch(v){
+        case 'v':
+          icon = 'fa-check-circle'
+          cls = 'success'
+          text = 'تائید شده'
+          break;
+        case 'n':
+          icon = 'fa-circle-o-notch fa-spin'
+          cls = 'default'
+          text = 'بررسی نشده'
+          break;
+        case 'r':
+        default:
+          icon = 'fa-times-circle'
+          cls = 'danger'
+          text = 'رد شده'
+          break;
+      }
+      return `<span class="${cls}"><i class="fa ${icon}" style="vertical-align: middle"></i> ${text}</span>`;
     }
   },
   router,
+  style,
   render: h => h(_main)
 }).$mount('#app')
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,39 @@
 import template from './template.pug'
 
 export default {
+  data(){
+    return {
+      tableFields: [
+        {
+          value: 'id',
+          text: '#'
+        },
+        {
+          value: 'national_code',
+          text: 'کد ملی'
+        },
+        {
+          value: 'create_date',
+          text: 'تاریخ ایجاد',
+          formatter: (v)=>{
+            return this.$root.dateFormat(v);
+          },
+          class: 'fv-hide-on-only-xs'
+        },
+        {
+          value: 'status',
+          text: 'وضعیت',
+          formatter: (v)=>{
+            return this.$root.statusFormat(v)
+          }
+        }
+      ]
+    }
+  },
+  computed: {
+    tableApi(){
+      return `v1/users`; 
+    }
+  },
   render: template.render
 }
